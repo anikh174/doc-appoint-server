8644e3c4837ef8b97a18fc9ed7ed7ce68c608137
anikh174 <hossainanik174@gmail.com>
2026-05-19 08:42:26 +0600

    add appointments retrieval API from database

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const cors = require('cors')
 dotenv.config()
 
-
 const app = express()
-const port = process.env.PORT
+app.use(cors());
+const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -28,12 +29,22 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const db = client.db('doc-appoint');
+    const appointmentsCollections = db.collection('appointments')
+
+
+    app.get('/appointments', async(req, res)=>{
+        const cursor = appointmentsCollections.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
