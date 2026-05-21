5c72894c8ea4d475ff94d3a48e534f6b8ce5cb8e
anikh174 <hossainanik174@gmail.com>
2026-05-21 21:06:23 +0600

    added verifyToken details page

--- index.js
+++ index.js
@@ -75,14 +75,14 @@ async function run() {
     const bookingCollections = db.collection("booking");
 
     // all-doctors
-    app.get("/doctors", verifyToken, async (req, res) => {
+    app.get("/doctors", async (req, res) => {
       const cursor = doctorsCollections.find();
       const result = await cursor.toArray();
       res.send(result);
     });
 
     // specific doctors
-    app.get("/doctors/:id", async (req, res) => {
+    app.get("/doctors/:id", verifyToken, async (req, res) => {
       const { id } = req.params;
       const Id = { _id: new ObjectId(id) };
       const result = await doctorsCollections.findOne(Id);
@@ -105,7 +105,7 @@ async function run() {
     });
 
     // get bookingInfo
-    app.get("/booking", verifyToken, async (req, res) => {
+    app.get("/booking", async (req, res) => {
       const result = await bookingCollections.find().toArray();
       res.json(result);
     });
